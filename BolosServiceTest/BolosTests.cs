06d35b8b234a96b889ad8364d18371265a332cfc
Epiquien1 <rikiepiquien@gmail.com>
2021-04-14 16:40:15 -0500

    Bowling prueba1

---
+++ BolosServiceTest/BolosTests.cs
@@ -0,0 +1,134 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using t1calidad;
+
+namespace BolosServiceTest
+{
+    [TestFixture]
+
+    public class BolosTests
+    {
+        BolosService bolosService;
+
+        [SetUp]
+
+        public void SetUpGame()
+        {
+            bolosService = new BolosService();
+        }
+         void CantidadRodadas(int lanzar, int pinos)
+        {
+            
+
+            for (int i = 0; i < lanzar; i++)
+            {
+                bolosService.Caida(pinos);
+            }
+
+        }
+
+
+        [Test]
+
+        public void RuedaPrimerJuego()
+        {
+
+           // var bolosService = new BolosService();
+            CantidadRodadas(20,0);
+
+           // Assert.AreEqual(bolosService.Puntaje(), Is.EqualTo(0));
+            Assert.That(bolosService.Puntaje(), Is.EqualTo(0));
+
+        }
+
+        [Test]
+
+        public void UnaRodada()
+        {
+           // var bolosService = new BolosService();
+
+            CantidadRodadas(20, 1); //Una en todas
+
+          
+            Assert.That(bolosService.Puntaje(), Is.EqualTo(20));
+        }
+
+        [Test]
+
+        public void RuedaFramaPrimerJuego()
+        {
+           // var bolosService = new BolosService();
+
+            bolosService.Caida(9);
+            bolosService.Caida(1);
+            CantidadRodadas(18, 1);  // una en todas 
+            // 18 + 11 = 29
+
+            Assert.That(bolosService.Puntaje(), Is.EqualTo(29));
+            
+        }
+
+        [Test]
+
+        public void RodadaTodoFrame()
+        {
+            var bolosService = new BolosService();
+            CantidadRodadas(21,5); 
+
+            Assert.That(bolosService.Puntaje(), Is.EqualTo(150));
+
+        }
+
+        [Test]
+        public void RodadaNueveUnaSpares()
+        {
+            
+            var bolosService = new BolosService();
+            for (int i = 0; i < 10; i++)
+            {
+                bolosService.Caida(9);
+                bolosService.Caida(1);
+
+               
+            }
+            bolosService.Caida(9);
+
+            Assert.That(bolosService.Puntaje(), Is.EqualTo(190));
+           
+
+
+        }
+
+        [Test]
+
+        public void RodadaJuegoPerfecto()
+        {
+            var bolosService = new BolosService();
+            CantidadRodadas(12,10);
+
+            Assert.That(bolosService.Puntaje(), Is.EqualTo(300));
+
+        }
+
+        [Test]
+        public void TypicalGame()
+        {
+            var bolosService = new BolosService();
+            bolosService.Caida(10);
+            bolosService.Caida(9); bolosService.Caida(10);
+            bolosService.Caida(5); bolosService.Caida(5);
+            bolosService.Caida(7); bolosService.Caida(2);
+            bolosService.Caida(10);
+            bolosService.Caida(10);
+            bolosService.Caida(10);
+            bolosService.Caida(9); bolosService.Caida(0);
+            bolosService.Caida(8); bolosService.Caida(2);
+            bolosService.Caida(9); bolosService.Caida(1); bolosService.Caida(10);
+
+            Assert.That(bolosService.Puntaje(), Is.EqualTo(187));
+
+        }
+    }
+}
---
+++ t1calidad/BolosService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace t1calidad
+{
+    public class BolosService
+    {
+        int contadorCaidas;
+        int[] PinoCae = new int[21]; //Máximo tiros 
+        
+        
+
+        public void Caida(int pinos)
+        {
+            PinoCae[contadorCaidas] = pinos;
+            contadorCaidas++;
+        }
+
+       
+
+        bool EsStrike(int IndiceFrame)
+        {
+            return PinoCae[IndiceFrame ] == 10;
+
+        }
+
+        bool EsSpare(int frameIndex)
+        {
+            return PinoCae[frameIndex] + PinoCae[frameIndex + 1] == 10;
+        }
+         
+        int StrikeBonus(int frameIndex)
+        {
+            return PinoCae[frameIndex + 1] + PinoCae[frameIndex + 2];
+            //return  PinoCae[frameIndex + 2];
+        }
+
+        int SpareBonus(int i)
+        {
+            return PinoCae[i + 2];
+        }
+
+        public int Puntaje()
+        {
+            int puntaje = 0;
+            int i = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if(EsStrike(i))
+                {
+                    puntaje += 10 + StrikeBonus(i);
+                    i += 1;
+                }
+
+            else if(EsSpare(i))
+                {
+                    puntaje += 10 + SpareBonus(i);
+                    i += 2;
+                }else
+                {
+                    puntaje += PinoCae[i] + PinoCae[i+1];
+                    i += 2;
+                }
+                    
+            }
+            return puntaje;
+        }
+    }
+}
